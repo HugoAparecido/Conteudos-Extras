b67904fb4dda76b8cdbd7116a3a4056db57622cb
HugoAparecido <120059026+HugoAparecido@users.noreply.github.com>
2023-07-23 19:19:23 -0300

    23/07/2023

--- Aplicação Javascript/Agenda/js/novo.js
+++ Aplicação Javascript/Agenda/js/novo.js
@@ -4,8 +4,23 @@ const f_nome = document.querySelector("#f_nome");
 const f_celular = document.querySelector("#f_celular");
 const f_email = document.querySelector("#f_email");
 const f_dtnasc = document.querySelector("#f_dtnasc");
-btn_gravar.addEventListener("click", (evt) => { console.log(f_nome.value); })
-const endpoint = "http://127.0.0.1:1880/contatos";
-fetch(endpoint).then(res => res.json()).then(res => {
-    console.log(res);
-})
+btn_gravar.addEventListener("click", (evt) => {
+    const dados = {
+        "f_nome": f_nome.value,
+        "f_celular": f_celular.value,
+        "f_email": f_email.value,
+        "f_dtnasc": f_dtnasc.value
+    }
+    const cabecalho = {
+        method: 'POST',
+        body: JSON.stringify(dados)
+    }
+    const endpoint = "http://127.0.0.1:1880/addcontatos";
+    fetch(endpoint, cabecalho).then(res => {
+        if (res.status == 200) {
+            console.log("OK");
+        } else {
+            alert("Erro ao gravar novo contato");
+        }
+    });
+});
