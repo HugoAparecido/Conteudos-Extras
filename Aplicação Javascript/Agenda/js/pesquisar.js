a4e190e1334d7eb9627c87dd6aeda3ebf6cc9c14
HugoAparecido <120059026+HugoAparecido@users.noreply.github.com>
2023-07-25 18:02:32 -0300

    25/07/2023

---
+++ Aplicação Javascript/Agenda/js/pesquisar.js
@@ -0,0 +1,15 @@
+const btn_pesq = document.querySelector("#btn_pesq");
+const f_txtpesq = document.querySelector("#f_txtpesq");
+btn_pesq.addEventListener("click", (evt) => {
+    const valorpesq = f_txtpesq.value;
+    if (valorpesq == "") {
+        alert("Digite a pesquisa");
+        f_txtpesq.focus();
+        return;
+    }
+    const f_pesq = document.querySelector("input[name=f_pesq]:checked").value;
+    const entpoint = `http://127.0.0.1:1880/pesquisarcontatos/${f_pesq}/${valorpesq}`;
+    fetch(entpoint).then(res => res.json()).then(res => {
+        console.log(res);
+    })
+});
